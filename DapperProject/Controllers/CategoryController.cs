217276ebf036a8c27844e8aba84869614a58703b
CicekHuseyin <95969675+CicekHuseyin@users.noreply.github.com>
2024-10-03 15:11:23 +0300

    Oluşturmuş olduğumuz Db nin verilerine projeden ulaştık ve CRUD operasyonlarını gerçekleştirdik.

---
+++ DapperProject/Controllers/CategoryController.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using DapperProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly string _connection = "Server=DESKTOP-KR485FT\\SQLEXPRESS; initial catalog=DbNews; integrated security=true";
+
+        public async Task<IActionResult> Index()
+        {
+            await using var connection = new SqlConnection(_connection);
+            var values = await connection.QueryAsync<ResultCategoryViewModel>("Select * from Categories");
+            //Yukarıdaki kodda sorgudan gelen değerler ile view model deki propertyler otomatik maplenmiş oluyor.
+            //QueryAsync Sql komutu yazmamızı sağlar.
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(ResultCategoryViewModel model)
+        {
+            await using var connection = new SqlConnection(_connection);
+            var query = $"Insert Into Categories (CategoryName,CategoryStatus) Values ('{model.CategoryName}','True')";
+            await connection.ExecuteAsync(query);
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            await using var connection = new SqlConnection(_connection);
+            var query = $"Delete From Categories Where CategoryID='{id}'";
+            await connection.ExecuteAsync(query);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(int id)
+        {
+            await using var connection = new SqlConnection(_connection);
+            var values = await connection.QueryFirstAsync<ResultCategoryViewModel>($"Select * from Categories Where CategoryID='{id}'");
+            return View(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(ResultCategoryViewModel model)
+        {
+            await using var connection = new SqlConnection(_connection);
+            var query = $"update Categories set CategoryName='{model.CategoryName}',CategoryStatus='{model.CategoryStatus}' where CategoryID='{model.CategoryID}'";
+            await connection.ExecuteAsync(query);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DapperProject/Models/ResultCategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DapperProject.Models
+{
+    public class ResultCategoryViewModel
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
